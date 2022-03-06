168de6664b54eaccd4e5c4beb31a8a5b5b80ac79
Francisco Silva Martín <fransilvamartin@gmail.com>
2022-03-06 04:21:53 +0100

    :memo: Documentación modificada

--- src/mainApp/MainApp.java
+++ src/mainApp/MainApp.java
@@ -2,6 +2,9 @@ package mainApp;
 
 public class MainApp {
 
+	// PROGRAMADOR: Francisco
+	// CLIENTE: Mohamed
+
 	// Inicio de la aplicación
 	public static void main(String[] args) {
 		new FutbolApp();
